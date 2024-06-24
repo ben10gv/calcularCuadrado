a2365e8f97e7f99e2f648a78cff19b5e36ffe9dd
Jesus Antonio Gonzalez Vazquez <ben10gv@hotmail.com>
2024-06-23 19:17:52 -0600

    23/06/2024 Solucion a la tarea de calcular area y perimetro de rectangulo

--- src/calculaareaperimetrorectangulo/CalculaAreaPerimetroRectangulo.java
+++ src/calculaareaperimetrorectangulo/CalculaAreaPerimetroRectangulo.java
@@ -4,6 +4,8 @@
  */
 package calculaareaperimetrorectangulo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ben10
@@ -14,7 +16,25 @@ public class CalculaAreaPerimetroRectangulo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*Declaracion de variable*/
+        int alto = 0;
+        int ancho = 0;
+        Scanner valores = new Scanner(System.in);
         
+        //Preguntar Alto
+        System.out.println("Proporcionar el alto:");
+        alto = valores.nextInt();
+        
+        //Preguntar Ancho
+        System.out.println("Proporcionar el ancho:");
+        ancho = valores.nextInt();
+        
+        //Calcular y mostrar resultados
+        int area = alto * ancho;
+        System.out.println("area = " + area);
+        
+        int perimetro = (alto + ancho) * 2;
+        System.out.println("perimetro = " + perimetro);
     }
     
 }
